671786c7a3f81c17a8c5601b24be0fb9f3b76a68
xenixux <kseniakonnova.ux@gmail.com>
2026-06-17 20:45:57 +0200

    shared button finalizado

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,26 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+  class = input<
+    | 'btn-primary'
+    | 'btn-secondary'
+    | 'btn-success'
+    | 'btn-danger'
+    | 'btn-warning'
+    | 'btn-info'
+    | 'btn-light'
+    | 'btn-dark'
+    | 'btn-link'
+  >('btn-primary');
+  showIconLeft = input<boolean>(false);
+  iconLeft = input<string>('');
+  text = input<string>('');
+  showIconRight = input<boolean>(false);
+  iconRight = input<string>('');
+}
