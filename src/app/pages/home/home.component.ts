aa887f82156f97f718a90fba0b13bc9f0d29e83a
xenixux <kseniakonnova.ux@gmail.com>
2026-06-19 20:32:15 +0200

    wip navbar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { AvatarComponent } from '../../shared/components/avatar/avatar.component';
+import { NavbarComponent } from '../../shared/layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent, AvatarComponent],
+  imports: [ButtonComponent, AvatarComponent, NavbarComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { AvatarComponent } from '../../components/avatar/avatar.component';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ButtonComponent, AvatarComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
