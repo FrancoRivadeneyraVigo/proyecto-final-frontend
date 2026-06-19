8dcad8cfd60714ba1731fbfe3f140af92ddced29
xenixux <kseniakonnova.ux@gmail.com>
2026-06-19 18:47:13 +0200

    pruebas avatar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
+import { AvatarComponent } from '../../shared/components/avatar/avatar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, AvatarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
