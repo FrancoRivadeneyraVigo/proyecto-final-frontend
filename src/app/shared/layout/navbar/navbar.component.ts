a89125a8b882463e3202fc5eea9e15d1e4671bb8
xenixux <kseniakonnova.ux@gmail.com>
2026-06-22 18:48:38 +0200

    feat: enlazar navbar con rutas de inicio, explorar y vender

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ButtonComponent } from '../../components/button/button.component';
 import { AvatarComponent } from '../../components/avatar/avatar.component';
 import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonComponent, AvatarComponent, RouterLink],
+  imports: [ButtonComponent, AvatarComponent, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
