0722004975eab22f0100d08eb8ddb6f905e6c26e
Francisco <franciscosanchezuceta@gmail.com>
2026-06-30 20:04:14 +0200

    feat: manage chat article status actions

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -110,4 +110,16 @@ export class ArticleService {
     return createdArticle;
   }
 
+
+  markAsReserved(id: number | string): Promise<{ article: IArticleDetail }> {
+    return lastValueFrom(this.httpClient.patch<{ article: IArticleDetail }>(`${this.articlesUrl}/${id}/reserved`, {}));
+  }
+
+  markAsPublished(id: number | string): Promise<{ article: IArticleDetail }> {
+    return lastValueFrom(this.httpClient.patch<{ article: IArticleDetail }>(`${this.articlesUrl}/${id}/published`, {}));
+  }
+
+  markAsSold(id: number | string): Promise<{ article: IArticleDetail }> {
+    return lastValueFrom(this.httpClient.patch<{ article: IArticleDetail }>(`${this.articlesUrl}/${id}/sold`, {}));
+  }
 }
