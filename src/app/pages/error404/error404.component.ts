ccdf03ce0a863af9e206122966020b9f010e2be6
NikyMikeCiolca <nikymikeciolca@gmail.com>
2026-07-03 22:23:28 +0200

    Pagina error 404 hecha

--- src/app/pages/error404/error404.component.ts
+++ src/app/pages/error404/error404.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { NavbarComponent } from '../../shared/layout/navbar/navbar.component';
+import { FooterComponent } from '../../shared/layout/footer/footer.component';
 
 @Component({
   selector: 'app-error404',
-  imports: [],
+  imports: [RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './error404.component.html',
   styleUrl: './error404.component.css',
 })
